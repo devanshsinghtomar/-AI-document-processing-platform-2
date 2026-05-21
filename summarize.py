529a7cb9d9924edb51cacf4864a93f21b29d38d8
Ananya-18-soni <soniananya1802@gmail.com>
2026-05-21 15:23:39 +0530

    Update summarize.py

--- summarize.py
+++ summarize.py
@@ -1,32 +1,19 @@
-
 from transformers import pipeline
 
-
-# LOAD SUMMARIZER MODEL
-
 summarizer = pipeline(
-
-    task="summarization",
-
-    model="facebook/bart-large-cnn"
+    "summarization",
+    model="sshleifer/distilbart-cnn-12-6"
 )
 
-
 def summarize_text(text):
-
-    # LIMIT VERY LARGE TEXT
 
     text = text[:2000]
 
     result = summarizer(
-
         text,
-
         max_length=120,
-
         min_length=30,
-
         do_sample=False
     )
 
-    return result[0]["summary_text"] 
+    return result[0]["summary_text"]
